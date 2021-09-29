8cba8197808527f7499aec6b8a1b21ec5bd1e8ae
Sockalingam <kanna.sockalingam@gmail.com>
2021-09-29 20:28:53 +0530

    add name input

--- App.js
+++ App.js
@@ -4,25 +4,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 import Home from './screens/home';
-import List from './screens/workouts';
+import Start from './screens/start';
 import Pushups from './screens/pushups';
 import Jumping from './screens/jumping';
 import Finish from './screens/finish';
+import Name from './screens/name';
 import FlashMessage from 'react-native-flash-message';
-import Pages from './screens/pages';
 
 const Stack = createNativeStackNavigator();
-
-
 
 function App() {
   return (
     
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="whatever" component={Pages} /> */}
         <Stack.Screen name="Welcome" component={Home} />
-        <Stack.Screen name="Start" component={List} />
+        <Stack.Screen name="Name" component={Name} />
+        <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Pushups" component={Pushups} />
         <Stack.Screen name="Jumping Jacks" component={Jumping} />
         <Stack.Screen name="Thank you" component={Finish} />
